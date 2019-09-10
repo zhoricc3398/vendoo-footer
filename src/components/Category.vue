d9d9da51744c87ca78bc97191a4516a5fdf0e0d3
g.sharabidze <g.sharabidze@vendoo.ge>
2019-09-10 12:47:29 +0400

    feat(source): მოვრჩი info და category

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -5,42 +5,82 @@
     <ul class="category__wrap">
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>ტექნიკა</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_4_.png"
+            alt="img1"
+          />
+          <span>ტექნიკა</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>ყველაფერი ბავშვებისთვის</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_5_.png"
+            alt="img2"
+          />
+          <span>ყველაფერი ბავშვებისთვის</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>თავის მოვლა</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_6_.png"
+            alt="img3"
+          />
+          <span>თავის მოვლა</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>ეზო და დასვენება</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_7_.png"
+            alt="img4"
+          />
+          <span>ეზო და დასვენება</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>სათამაშოები</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_1_.png"
+            alt="img5"
+          />
+          <span>სათამაშოები</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>საყოფაცხოვრებო ქიმია</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_2_.png"
+            alt="img6"
+          />
+          <span>საყოფაცხოვრებო ქიმია</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>სახლი</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/asdasd_12_3_.png"
+            alt="img7"
+          />
+          <span>სახლი</span></a
         >
       </li>
       <li class="category__list-item">
         <a href="#"
-          ><img class="category__image" src="" alt="" /> <span>სასმელები</span></a
+          ><img
+            class="category__image"
+            src="../assets/images/12-double-cask-gold_copy.png"
+            alt="img8"
+          />
+          <span>სასმელები</span></a
         >
       </li>
     </ul>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="info">
-    <a href="#">
+    <div class="info__main">
       <img src="../assets/images/back.png" alt="" />
-    </a>
-    <div class="info__wrap">
-      <div class="info__left"></div>
-      <div class="info__right"></div>
+      <div class="info__wrap">
+        <div class="info__left">
+          <h4>ყველაფერი მუსიკის მოსასმენად</h4>
+          <p>შეარჩიეთ საუკეთესო</p>
+          <a href="#">შეარჩიე</a>
+        </div>
+        <div class="info__right">
+          <ul class="info__list-wrap">
+            <li class="info__list-item">
+              <a href="#"
+                ><img
+                  src="../assets/images/dinamiki-purple.png"
+                  alt="img1"
+                  class="info__list-image"
+              /></a>
+            </li>
+            <li class="info__list-item">
+              <a href="#"
+                ><img
+                  src="../assets/images/22.png"
+                  alt="img2"
+                  class="info__list-image"
+              /></a>
+            </li>
+            <li class="info__list-item">
+              <a href="#"
+                ><img
+                  src="../assets/images/3.png"
+                  alt="img3"
+                  class="info__list-image"
+              /></a>
+            </li>
+            <li class="info__list-item">
+              <a href="#"
+                ><img
+                  src="../assets/images/4_1.png"
+                  alt="img4"
+                  class="info__list-image"
+              /></a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
